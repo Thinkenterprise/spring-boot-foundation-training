3f9aff45736c1c0f96620968de446aedd34fffb2
thinkenterprise <michael.schaefer@msg-systems.com>
2020-04-10 14:48:49 +0200

    delete old directory structure

--- 04-spring-boot-foundation-data/spring-boot-foundation-data-final/src/main/resources/data-h2.sql
+++
@@ -1,3 +0,0 @@
-insert into route(id, flight_Number, departure, destination) values(101, 'LH7902','MUC','IAH');
-insert into route(id, flight_Number, departure, destination) values(102, 'LH1602','MUC','IBZ');
-insert into route(id, flight_Number, departure, destination) values(103, 'LH401','FRA','NYC');
